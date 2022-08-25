338eb4dc918b3683b5709bb35e9180fc8b7dce88
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2022-08-25 16:59:40 -0300

    create Post page UI

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,3 +1,69 @@
+import {
+  FaCalendarDay,
+  FaGithub,
+  FaComment,
+  FaExternalLinkAlt,
+  FaChevronLeft,
+} from 'react-icons/fa'
+import { PostContainer, PostContent, PostDataCard } from './styles'
+
 export function Post() {
-  return <h1>Post</h1>
+  return (
+    <PostContainer>
+      <PostDataCard>
+        <header>
+          <a href="">
+            <span>
+              <FaChevronLeft size={12} />
+              VOLTAR
+            </span>
+          </a>
+
+          <a href="">
+            <span>
+              VER NO GITHUB <FaExternalLinkAlt size={12} />
+            </span>
+          </a>
+        </header>
+
+        <h1>JavaScript data types and data structures</h1>
+
+        <div>
+          <span>
+            <FaGithub size={18} />
+            rodolfomariano
+          </span>
+
+          <span>
+            <FaCalendarDay size={18} />
+            Há 1 dia
+          </span>
+
+          <span>
+            <FaComment size={18} />3 comentários
+          </span>
+        </div>
+      </PostDataCard>
+
+      <PostContent>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn.
+        </p>
+
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn.
+        </p>
+      </PostContent>
+    </PostContainer>
+  )
 }
---
+++ src/pages/Post/styles.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const PostDataCard = styled.div`
+  width: 100%;
+  background-color: ${({ theme }) => theme['base-profile']};
+  border-radius: 10px;
+  padding: 32px;
+
+  header {
+    margin-bottom: 1.25rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a {
+      span {
+        color: ${({ theme }) => theme['brand-blue']};
+        font-size: 12px;
+        border-bottom: 1px solid transparent;
+        text-transform: uppercase;
+        transition: 300ms;
+
+        display: flex;
+        align-items: center;
+        gap: 8px;
+
+        &:hover {
+          border-bottom: 1px solid ${({ theme }) => theme['brand-blue']};
+        }
+      }
+    }
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme['base-title']};
+    line-height: 130%;
+  }
+
+  div {
+    margin-top: 8px;
+
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    flex-wrap: wrap;
+
+    span {
+      color: ${({ theme }) => theme['base-label']};
+
+      flex: none;
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      svg {
+      }
+    }
+  }
+`
+export const PostContent = styled.div`
+  padding: 40px 2rem;
+
+  p {
+    font-size: 1rem;
+    color: ${({ theme }) => theme['base-text']};
+    line-height: 160%;
+  }
+
+  p + p {
+    margin-top: 2rem;
+  }
+`
